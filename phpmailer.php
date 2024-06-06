a1ca50b899cb0b7510621f89ba2f4afb47c90f1e
ACMYSistemas <acmysistemas@gmail.com>
2024-06-06 09:26:24 -0500

    Sexta Subida

--- phpmailer.php
+++ phpmailer.php
@@ -1,6 +1,7 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] != 'POST' ){
-    header("Location: index.html" );
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header("Location: index.html");
+    exit;
 }
 
 require 'phpmailer/PHPMailer.php';
@@ -9,13 +10,11 @@ require 'phpmailer/Exception.php';
 use PHPMailer\PHPMailer\PHPMailer;
 
 $nombre = $_POST['nombre'];
-//$apellido = $_POST['apellido'];
 $email = $_POST['email'];
 $asunto = $_POST['titulo'];
 $mensaje = $_POST['mensaje'];
 
-if( empty(trim($nombre)) ) $nombre = 'anonimo';
-if( empty(trim($apellido)) ) $apellido = '';
+if (empty(trim($nombre))) $nombre = 'anonimo';
 
 $body = <<<HTML
     <h1>Contacto desde la web</h1>
@@ -25,16 +24,15 @@ $body = <<<HTML
 HTML;
 
 $mailer = new PHPMailer();
-$mailer->setFrom( $email, "$nombre" );
-$mailer->addAddress('acmysistemas@gmail.com','ACMYSistemas');
+$mailer->setFrom($email, $nombre);
+$mailer->addAddress('acmysistemas@gmail.com', 'ACMYSistemas');
 $mailer->Subject = "Mensaje Web: $asunto";
 $mailer->msgHTML($body);
 $mailer->AltBody = strip_tags($body);
 $mailer->CharSet = 'UTF-8';
 
+$rta = $mailer->send();
 
-
-$rta = $mailer->send( );
-
-//var_dump($rta);
-header("Location: index.html" );
+header("Location: index.html");
+exit;
+?>
